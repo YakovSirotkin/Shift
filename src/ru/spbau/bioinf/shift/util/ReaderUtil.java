fbf4ec78a815791606e394cd3d62e3d18d58587c
Yakov Sirotkin <yasha@telamon.ru>
2011-07-11 15:35:13 +0400

    Add createOutputFile() method. http://ablab.myjetbrains.com/youtrack/issue/SHIFT-5

--- src/ru/spbau/bioinf/shift/util/ReaderUtil.java
+++ src/ru/spbau/bioinf/shift/util/ReaderUtil.java
@@ -4,8 +4,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -55,7 +59,14 @@ public class ReaderUtil {
         return "true".equalsIgnoreCase(prop.getProperty(key));
     }
 
-    public static BufferedReader getBufferedReader(File file) throws FileNotFoundException {
-        return new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+    public static BufferedReader getBufferedReader(File file)
+         throws UnsupportedEncodingException, FileNotFoundException {
+        return new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
+    }
+
+    public static PrintWriter createOutputFile(File file)
+            throws UnsupportedEncodingException, FileNotFoundException {
+        return new PrintWriter(new OutputStreamWriter(
+                new FileOutputStream(file), "UTF-8"));
     }
 }
