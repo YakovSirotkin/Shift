ce3a2a17b6233d5cf089778ab5f189fda3c8e65c
Yakov Sirotkin <yasha@telamon.ru>
2011-07-11 16:03:24 +0400

    Add getSpectrums() and getProteins(). http://ablab.myjetbrains.com/youtrack/issue/SHIFT-5

--- src/ru/spbau/bioinf/shift/Configuration.java
+++ src/ru/spbau/bioinf/shift/Configuration.java
@@ -1,6 +1,13 @@
 package ru.spbau.bioinf.shift;
 
+import ru.spbau.bioinf.shift.util.ReaderUtil;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 public class Configuration {
 
@@ -62,5 +69,22 @@ public class Configuration {
 
     public File getSpectrumXmlFile(Spectrum spectrum) {
         return new File(xmlSpectrumsDir, "spectrum" + spectrum.getId() + ".xml");
+    }
+
+    public List<Spectrum> getSpectrums() throws IOException {
+        BufferedReader input = ReaderUtil.getBufferedReader(getSpectrumsFile());
+
+        Properties properties;
+        List<Spectrum> spectrums = new ArrayList<Spectrum>();
+        while ((properties = ReaderUtil.readPropertiesUntil(input, "PRECURSOR_MASS")).size() > 0) {
+            Spectrum spectrum = new Spectrum(properties, input);
+            spectrums.add(spectrum);
+        }
+        return spectrums;
+    }
+
+    public List<Protein> getProteins() throws Exception {
+        ProteinDatabaseReader databaseReader = new ProteinDatabaseReader(getProteinDatabaseFile());
+        return databaseReader.getProteins();
     }
 }
