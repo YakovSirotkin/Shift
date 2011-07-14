632a44f7866b2b0ec57af71b01cbf1190f9ef3b7
Yakov Sirotkin <yasha@telamon.ru>
2011-07-14 08:29:40 +0400

    Support creating spectrum with removed shared peaks. http://ablab.myjetbrains.com/youtrack/issue/SHIFT-6

--- src/ru/spbau/bioinf/shift/Spectrum.java
+++ src/ru/spbau/bioinf/shift/Spectrum.java
@@ -18,29 +18,34 @@ public class Spectrum {
     private int id;
     private List<Peak> peaks = new ArrayList<Peak>();
     private String scans;
-    private float precursorMz;
+    private double precursorMz;
     private int precursorCharge;
-    private float precursorMass;
+    private double precursorMass;
 
     private double[] data = null;
 
     public Spectrum(Properties prop, BufferedReader input) throws IOException {
         id = ReaderUtil.getIntValue(prop, "ID");
         scans = ReaderUtil.getValue(prop, "SCANS");
-        precursorMz = ReaderUtil.getFloatValue(prop, "PRECURSOR_MZ");
+        precursorMz = ReaderUtil.getDoubleValue(prop, "PRECURSOR_MZ");
         precursorCharge = ReaderUtil.getIntValue(prop, "PRECURSOR_CHARGE");
-        precursorMass = ReaderUtil.getFloatValue(prop, "PRECURSOR_MASS");
+        precursorMass = ReaderUtil.getDoubleValue(prop, "PRECURSOR_MASS");
         List<String[]> datas = ReaderUtil.readDataUntil(input, "END IONS");
         for (String[] data : datas) {
             peaks.add(new Peak(Float.parseFloat(data[0]), Float.parseFloat(data[1]), Integer.parseInt(data[2])));
         }
     }
 
+    public Spectrum(List<Peak> peaks, double precursorMass) {
+        this.precursorMass = precursorMass;
+        this.peaks = peaks;
+    }
+
     public int getId() {
         return id;
     }
 
-    public float getPrecursorMass() {
+    public double getPrecursorMass() {
         return precursorMass;
     }
 
@@ -57,11 +62,7 @@ public class Spectrum {
             List<Double> d = new ArrayList<Double>();
             for (Peak peak : peaks) {
                 double p = peak.getMonoisotopicMass();
-                double r = precursorMass - p;
-                d.addAll(Arrays.asList(
-                        p - 1, p, p + 1, p + Consts.AMMONIA, p + Consts.WATER,
-                        r - 1, r, r + 1, r - Consts.AMMONIA, r - Consts.WATER)
-                    );
+                d.addAll(getModifications(p));
             }
             Collections.sort(d);
             d = ProteinFinder.merge(d);
@@ -72,6 +73,36 @@ public class Spectrum {
         }
 
         return data;
+    }
+
+    private List<Double> getModifications(double p) {
+        double r = precursorMass - p;
+        return Arrays.asList(
+                p - 1, p, p + 1, p + Consts.AMMONIA, p + Consts.WATER,
+                r - 1, r, r + 1, r - Consts.AMMONIA, r - Consts.WATER);
+    }
+
+    public Spectrum getLeft(double[] p, double shift) {
+        List<Peak> left = new ArrayList<Peak>();
+        for (Peak peak : peaks) {
+            List<Double> mods = getModifications(peak.getMonoisotopicMass());
+            boolean found = false;
+            for (double mod : mods) {
+                for (double v : p) {
+                    if (Math.abs(mod - v + shift) < 0.1) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (found) {
+                    break;
+                }
+            }
+            if (!found) {
+                left.add(peak);
+            }
+        }
+        return new Spectrum(left, precursorMass);
     }
 
     public void clearData() {
