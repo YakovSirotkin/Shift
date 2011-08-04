0c83fe018febb958511d5b9500ebc62c55574da3
Yakov Sirotkin <yasha@telamon.ru>
2011-08-04 13:21:27 +0400

    Initial version. http://ablab.myjetbrains.com/youtrack/issue/SHIFT-14

---
+++ src/ru/spbau/bioinf/shift/Modifications.java
@@ -0,0 +1,25 @@
+package ru.spbau.bioinf.shift;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum Modifications {
+
+    CORE(){
+        @Override
+        List<Double> getModifications(double p, double precursorMass) {
+            double r = precursorMass - p - Consts.WATER;
+            return Arrays.asList(p, r);
+        }
+    },
+    ADDITIONAL(){
+        @Override
+        List<Double> getModifications(double p, double precursorMass) {
+            double r = precursorMass - p - Consts.WATER;
+            return Arrays.asList(p-1, p+1, p + Consts.CO, r-1, r+1, r - Consts.AMMONIA, r - Consts.WATER, r + Consts.WATER);
+        }
+    };
+
+    abstract List<Double> getModifications(double p, double precursorMass);
+
+}
