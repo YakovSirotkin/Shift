8ec3504fc5153a35174c0f342515acbc3279849d
Yakov Sirotkin <yasha@telamon.ru>
2011-08-19 14:18:11 +0400

    Introduce Y_SHIFT and support B-only ions set. http://ablab.myjetbrains.com/youtrack/issue/SHIFT-27

--- src/ru/spbau/bioinf/shift/Modifications.java
+++ src/ru/spbau/bioinf/shift/Modifications.java
@@ -5,20 +5,29 @@ import java.util.List;
 
 public enum Modifications {
 
+    BONLY(){
+        @Override
+        List<Double> getModifications(double p, double precursorMass) {
+            return Arrays.asList(p);
+        }
+    },
+
     CORE(){
         @Override
         List<Double> getModifications(double p, double precursorMass) {
-            double r = precursorMass - p - Consts.WATER;
+            double r = precursorMass - p - Y_SHIFT;
             return Arrays.asList(p, r);
         }
     },
     ADDITIONAL(){
         @Override
         List<Double> getModifications(double p, double precursorMass) {
-            double r = precursorMass - p - Consts.WATER;
+            double r = precursorMass - p - Y_SHIFT;
             return Arrays.asList(p-1, p+1, p + Consts.CO, p + Consts.WATER, r-1, r+1, r - Consts.AMMONIA, r - Consts.WATER, r + Consts.WATER);
         }
     };
+
+    private static final double Y_SHIFT = 0.035;
 
     abstract List<Double> getModifications(double p, double precursorMass);
 
